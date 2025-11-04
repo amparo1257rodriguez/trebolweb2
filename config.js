bed8f64fb17772766bb976d3f706d08d9c8626c0
amparo1257rodriguez <amparo1257rodriguez@gmail.com>
2025-11-04 18:00:15 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 // Archivo de configuración para el número de línea de WhatsApp
-export const NUMERO_DE_LINEA = '5491176116901';
+export const NUMERO_DE_LINEA = '5491176116974';
 // Configuración de Meta Pixel
 export const META_PIXEL_ID = '1532282187735196';
 
 export const META_EVENT_NAME = 'ClickTrebol';
+
